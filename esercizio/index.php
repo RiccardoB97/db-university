3243035ddbc1e0b6ae3c0ffb0aedf2a5e177d4a8
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-06 13:31:17 +0200

    Checked for connection in database and added query for db

--- esercizio/index.php
+++ esercizio/index.php
@@ -8,6 +8,14 @@ define('DB_NAME', 'university');
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 
+if($connection && $connection->connect_error){
+    echo "Error connecting, connection failed!". $connection->connect_error;
+} else {
+    echo 'Connection succesfull!';
+}
+
+$query = "SELECT * FROM courses";
+$result = $connection->query($query);
 
 
 
